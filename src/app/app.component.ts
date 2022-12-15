6805f0746d2631991f3ae014add30ec4d208bd45
RodResq <rodrigoresque@yahoo.com.br>
2022-12-15 15:19:55 -0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'model-driven-forms-labs';
+
+  langs: string[] = ["English", "French", "German"];
 }
