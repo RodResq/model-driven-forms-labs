43d827b7c08a25bdb0416f2a6a26071fd379c6a4
RodResq <rodrigoresque@yahoo.com.br>
2022-12-23 10:33:06 -0300

    updt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  myForm: FormGroup
 
   langs: string[] = [
     'English',
     'French',
     'German'
   ]
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      name: new FormGroup({
+        firstname: new FormControl(null, Validators.required),
+        lastname: new FormControl(null, Validators.required),
+      }),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("[^ @]*@[^ @]*"),
+      ], this.forbiddenEmail.bind(this)),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8)
+      ]),
+      language: new FormControl(null, Validators.required)
+    });
+  }
+
+  forbiddenEmail(controls: FormControl): Promise<any> | Observable<any> {
+    const promisse = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (controls.value === 'test@test.com') {
+          resolve({'emailIsFOrbidden': true});
+        } else {
+          resolve(null)
+        }
+      }, 1500)
+    });
+    return promisse;
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
+  }
 
 }
